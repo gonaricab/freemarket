bf0804f80c15803fcf21a854fbbf5b6b8285b3dc
gonzalo <gonaricab@gmail.com>
2022-12-26 15:04:29 -0300

    agregue lista

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <div>
+          <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Servicios</li>
+          </ul>
+        </div>
+    )
+};
+export default NavBar;
